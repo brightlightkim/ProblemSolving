866b80f8f9636fec104edb63e4c59b03bde73ab9
brightlightkim <taeyangk95@naver.com>
2022-08-08 13:06:49 -0600

    Solved few quizes

--- easy/1+/vauvau.py
+++ easy/1+/vauvau.py
@@ -2,9 +2,18 @@ dogs = list(map(int, input().split()))
 mens = list(map(int, input().split()))
 
 for i in range(len(mens)):
-    if mens[i] <= dogs[0] and mens[i] <= dogs[2]:
+    firstVal = mens[i] % (dogs[0] + dogs[1])
+    secondVal = mens[i] % (dogs[2] + dogs[3])
+    isFirst = False
+    isSecond = False
+    if firstVal <= dogs[0] and firstVal != 0:
+        isFirst = True
+    if secondVal <= dogs[2] and secondVal != 0:
+        isSecond = True
+    if isFirst and isSecond:
         print('both')
-    elif mens[i] <= dogs[0] or mens[i] <= dogs[2]:
+    elif isFirst or isSecond:
         print('one')
     else:
         print('none')
+
